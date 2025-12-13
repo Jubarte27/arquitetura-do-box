10378ee98fc7d4b267cfb7a94a268f922610394a
Matheus Machado <matheusmazar@gmail.com>
2025-12-13 12:25:06 -0300

    Leitura superpop

--- WORK/main.asm
+++ WORK/main.asm
@@ -1,120 +1,223 @@
+;====================================================================
+; Macros
+SaveRegs macro regs:VARARG
+	local reg
+	FOR   reg, <regs>
+		push reg
+	ENDM
+ENDM
+; RestoreRegs - Macro to generate a pop instruction for registers
+; saved by the SaveRegs macro. Restores one group of registers.
+RestoreRegs macro regs:VARARG
+	local reg
+	FOR   reg, <regs>
+		pop reg
+	ENDM
+ENDM
+
+
+printf_c macro string:req
+	SaveRegs ax,   dx
+	FORC     char, <string>
+		mov ah, 02h     ;; Select DOS Print Char function
+		mov dl, '&char' ;; Select ASCII char
+		int 21h         ;; Call DOS
+	ENDM
+	RestoreRegs dx, ax
+ENDM
+
+print_Pair macro line:req, col:req
+	SaveRegs ax
+
+    printf_c <(>
+    mov      ax, line
+    call     printf_w
+    printf_c <:>
+    mov      ax, col
+    call     printf_w
+    printf_c <)>
+
+	RestoreRegs ax
+ENDM
+
+print_FilePosition macro
+	print_Pair FileLine, FileCol
+ENDM
+
+print_TotalRowCol macro
+	print_Pair TotalRow, TotalCol
+endm
+
+HandleCR macro
+	call PeekChar
+	mov  bh, PeekBuffer
+	.IF  bh != LF
+		jmp ErrorUnexpectedChar
+	.ENDIF
+endm
+
+;====================================================================
+; Program
+
 	.model small
 	.code
 	.startup
 Main:
-	; abre arquivo
 	call OpenFile
-Again:
-    ; lê um caractere do arquivo
+	
+MainLoop:
     call ReadChar
-
-	; verifica se terminou o arquivo
-    .IF ax == 0
-        mov al, 0
-        jmp CloseAndFinal
+    .IF  ax == 0  ; EOF
+		mov ax, TotalCol
+		dec ax             ; ax = N
+		.IF TotalRow != ax
+			jmp ErrorRowCount
+		.ENDIF
+        jmp ExitSuccess
     .ENDIF
 
 	mov bl, FileBuffer
 
     .IF bl == COL_SEPARATOR
         inc Col
-        inc TotalCol
     .ELSEIF bl == LF
-        inc Row
-        inc TotalRow
-        mov Col, 0
-
         inc FileLine
-        mov FileCol, 0
+        mov FileCol, 1
+		;====================================================================
+		; On a new line, the number of columns should always be the same
+		mov ax, Col
+		.IF Row == 0
+			mov TotalCol, ax
+		.ELSEIF TotalCol != ax
+			jmp ErrorColumnCount
+		.ENDIF
+		;====================================================================
+		; If next line is empty, all next lines should be empty
+		call PeekChar
+		mov  bh, PeekBuffer
+		.IF  bh == LF || bh == CR
+			call ReadEmptyLines
+		;====================================================================
+		; Otherwise, next line must have data
+		.ELSE
+			inc Row
+			inc TotalRow
+			mov Col, 0
+		.ENDIF
     .ELSEIF bl == CR ; accept cr only before lf
-        call PeekChar
-        mov  bh, PeekBuffer
-        .IF  bh != LF
-	        jmp ErrorUnexpectedChar
-        .ENDIF
+        HandleCR
     .ELSEIF bl == '-'
         ;do something
-	.ELSEIF bl > '9' || bl < '0'
+	.ELSEIF bl <= '9' && bl >= '0'
+		sub bl, '0'
+	.ELSE
         jmp ErrorUnexpectedChar
     .ENDIF
-
-	sub bl, '0'
-
 NextLoop:
-	jmp Again
+	jmp MainLoop
 
-CloseAndFinal:
-	; fecha arquivo
-	mov ah, 3eh
-	mov bx, FileHandle
-	int 21h
-	jmp Final
+;====================================================================
+; Exiting
 
-Final:
+ExitSuccess:
+	mov al, 0
+	jmp ExitAndClose
+
+ExitFailure:
+	mov al, 1
+	jmp ExitAndClose
+
+ExitAndClose:
+	.IF FileIsOpen
+		mov ah,         3eh
+		mov bx,         FileHandle
+		int 21h
+		mov FileIsOpen, 1          ; file is now closed
+	.ENDIF
 	.exit
 
+;====================================================================
+; Error reporting
 
 ErrorOpen:
-	lea  bx, MsgErroOpenFile
-	call printf_s
-	mov  al, 1
-	jmp  Final
+	printf_c <Erro na abertura do arquivo.>
+	jmp      ExitFailure
 
 ErrorRead:
-    lea  bx, MsgErroReadFile
-	call printf_s
-	mov  al, 1
-	jmp  CloseAndFinal
+    printf_c <Erro na leitura do arquivo.>
+	jmp      ExitFailure
+
+ErrorColumnCount:
+    print_FilePosition
+    printf_c < Erro: a quantidade de colunas deve ser igual em todas as linhas.>
+	jmp      ExitFailure
+
+ErrorRowCount:
+    printf_c <Erro: a quantidade de linhas deve ser 1 a menos que a quantidade de colunas. O encontrado foi: >
+	print_TotalRowCol
+	jmp      ExitFailure
 
 ErrorUnexpectedChar:
     mov TheUnexpectedChar, bl
 
-    mov  dl, '('
-    call putc
-    mov  ax, FileLine
-    call printf_w
-    mov  dl, ':'
-    call putc
-    mov  ax, FileCol
-    call printf_w
-    mov  dl, ')'
-    call putc
+    print_FilePosition
 
-    lea  bx, MsgUnexpectedCharacter
-	call printf_s
-    lea  bx, TheUnexpectedChar
-	call printf_s
-	mov  al, 1
-	jmp  CloseAndFinal
+    printf_c < Erro: caracter inexperado: >
+    lea      bx, TheUnexpectedChar
+	call     printf_s
+
+	jmp ExitFailure
 
 
 ;====================================================================
 ; Functions
 
-OpenFile proc near
-    mov al,         0
-	lea dx,         FileName
-	mov ah,         3dh
-	int 21h
-	jc  ErrorOpen
-    mov FileHandle, ax
+ReadEmptyLines proc near
+	SaveRegs ax, bx
+    call     ReadChar
+	.WHILE   ax != 0
+		mov bl, FileBuffer
+
+		.IF bl == LF
+			inc FileLine
+			mov FileCol, 1
+		.ELSEIF bl == CR
+			HandleCR
+		.ELSE
+			jmp ErrorUnexpectedChar
+		.ENDIF
+
+		call ReadChar
+	.ENDW
+    RestoreRegs bx, ax
+	ret
+ReadEmptyLines endp
+
+OpenFile       proc near
+	SaveRegs    ax,         dx
+    mov         al,         0
+	lea         dx,         FileName
+	mov         ah,         3dh
+	int         21h
+	jc          ErrorOpen
+    mov         FileHandle, ax
+	mov         FileIsOpen, 0        ; 0 means it is open
+	RestoreRegs dx,         ax
     ret
 OpenFile endp
 
 ReadChar proc near
-    push dx
-	lea  dx, FileBuffer
-    call ReadCharToDX
-    pop  dx
+    SaveRegs dx
+	lea      dx, FileBuffer
+    call     ReadCharToDX
     
-    inc FileCol
-
+    inc         FileCol
+    RestoreRegs dx
     ret
 ReadChar     endp
 
 ReadCharToDX proc near
-    push bx
-    push cx
+	SaveRegs bx, cx
 
 	mov bx, FileHandle
 	mov ah, 3fh
@@ -128,16 +231,12 @@ ReadCharToDX proc near
         mov byte ptr[bx], 0
     .ENDIF
 
-    pop cx
-    pop bx
+    RestoreRegs cx, bx
     ret
 ReadCharToDX endp
 
 PeekChar     proc near
-    push ax
-    push bx
-    push cx
-    push dx
+    SaveRegs ax, bx, cx, dx
 
 	lea  dx, PeekBuffer
 	call ReadCharToDX
@@ -145,25 +244,16 @@ PeekChar     proc near
     ; move back by one byte
     mov bx, FileHandle
     mov ah, 42h
-    mov cx, 0
+    mov cx, 0FFFFh ; Means dx is negative
     mov dx, -1
     mov al, 1
     int 21h
+	jc ErrorRead
 
 EndPeek:
-    pop dx
-    pop cx
-    pop bx
-    pop ax
+	RestoreRegs dx, cx, bx, ax
     ret
 PeekChar endp
-
-; prints the char in dl
-putc     proc near
-    mov ah, 02h
-	int 21H
-    ret
-putc     endp
 
 ;====================================================================
 ; A partir daqui, estão as funções já desenvolvidas
@@ -177,6 +267,8 @@ putc     endp
 ;printf_s(char *s -> bx)
 ;--------------------------------------------------------------------
 printf_s proc	near
+	; SaveRegs ax, bx, dx
+	
 	mov dl, [bx]
 	cmp dl, 0
 	je  ps_1
@@ -190,6 +282,7 @@ printf_s proc	near
 	jmp printf_s
 
 ps_1:
+	; RestoreRegs dx, bx, ax
 	ret
 printf_s endp
 
@@ -199,6 +292,7 @@ printf_s endp
 ;printf("%
 ;--------------------------------------------------------------------
 printf_w proc	near
+	; SaveRegs bx
 	; sprintf_w(ax, BufferWRWORD)
 	lea  bx, BufferWRWORD
 	call sprintf_w
@@ -207,6 +301,7 @@ printf_w proc	near
 	lea  bx, BufferWRWORD
 	call printf_s
 	
+	; RestoreRegs bx
 	ret
 printf_w  endp
 
@@ -216,6 +311,7 @@ printf_w  endp
 ; sprintf(string->bx, "%d", n->ax)
 ;--------------------------------------------------------------------
 sprintf_w proc	near
+	; SaveRegs ax,bx,cx,dx
 	mov sw_n, ax
 	mov cx,   5
 	mov sw_m, 10000
@@ -257,6 +353,7 @@ sw_continue:
 sw_continua2:
 
 	mov byte ptr[bx], 0
+	; RestoreRegs dx,cx,bx,ax
 	ret
 sprintf_w endp
 
@@ -272,38 +369,36 @@ COL_SEPARATOR equ SEMI
 
 	.data
 
-BuffSize               equ 100                                       ; tam. máximo dos dados lidos no buffer
-FileName               db  "MAT.TXT",0                               ; Nome do arquivo a ser lido
-FileBuffer             db  BuffSize dup (?)                          ; Buffer de leitura do arquivo
-FileHandle             dw  0                                         ; Handler do arquivo
-FileNameBuffer         db  150 dup (?)
-caractere              db  0
+BuffSize          equ 100              ; tam. máximo dos dados lidos no buffer
+FileName          db  "MAT.TXT",0      ; Nome do arquivo a ser lido
+FileBuffer        db  BuffSize dup (?) ; Buffer de leitura do arquivo
+FileHandle        dw  0                ; Handler do arquivo
+FileIsOpen        db  1                ; closed at the start
+FileNameBuffer    db  150 dup (?)
+caractere         db  0
 
-MsgErroOpenFile        db  "Erro na abertura do arquivo.", CR, LF, 0
-MsgErroReadFile        db  "Erro na leitura do arquivo.", CR, LF, 0
-MsgUnexpectedCharacter db  " Erro: caracter inexperado: ",QUOT, 0
-MsgCRLF                db  CR, LF, 0
+MsgCRLF           db  CR, LF, 0
 
 ; Used on PeekChar
-PeekBuffer             db  ?
+PeekBuffer        db  ?
 ; Used when reporting the error
-TheUnexpectedChar      db  0,QUOT,0
+TheUnexpectedChar db  0,QUOT,0
 ; Variável interna usada na rotina printf_w
-BufferWRWORD           db  10 dup (?)
+BufferWRWORD      db  10 dup (?)
 ; Variaveis para uso interno na função sprintf_w
-sw_n                   dw  0
-sw_f                   db  0
-sw_m                   dw  0
+sw_n              dw  0
+sw_f              db  0
+sw_m              dw  0
 ; used in main
-FileCol                dw  1
-FileLine               dw  1
+FileCol           dw  1
+FileLine          dw  1
 
-Row                    dw  0
-Col                    dw  0
-TotalRow               dw  1
-TotalCol               dw  1
+Row               dw  0
+Col               dw  0
+TotalRow          dw  0
+TotalCol          dw  0
 
-Matrix                 dw  0
+Matrix            dw  0
 
 ;--------------------------------------------------------------------
 end
